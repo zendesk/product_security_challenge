a5ac6a000747c430767e7ae91d038f997cde7ff3
Kiesia Carmine <kiesia@sendle.com>
2020-01-11 18:45:50 +1100

    disable unused rails functionality

--- authenticator/config/environments/development.rb
+++ authenticator/config/environments/development.rb
@@ -28,9 +28,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
