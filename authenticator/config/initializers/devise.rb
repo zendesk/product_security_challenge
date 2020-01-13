01700d28a58e74baa1b804939444328681782177
Kiesia Carmine <kiesia@sendle.com>
2020-01-13 20:57:22 +1100

    relax stretches

--- authenticator/config/initializers/devise.rb
+++ authenticator/config/initializers/devise.rb
@@ -111,7 +111,7 @@ Devise.setup do |config|
   # a value less than 10 in other environments. Note that, for bcrypt (the default
   # algorithm), the cost increases exponentially with the number of stretches (e.g.
   # a value of 20 is already extremely slow: approx. 60 seconds for 1 calculation).
-  config.stretches = Rails.env.test? ? 1 : 14
+  config.stretches = Rails.env.test? ? 1 : 13
 
   # Set up a pepper to generate the hashed password.
   config.pepper = ENV.fetch("DEVISE_PEPPER")
